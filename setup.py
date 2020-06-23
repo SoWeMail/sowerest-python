db437193e903daa658f9ede7961a2b60a57c6c0f
Fourat ZOUARI <fourat@gmail.com>
2020-06-23 22:35:07 +0100

    Conforming repo files.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os
 from distutils.file_util import copy_file
 from setuptools import setup
 
+base_gh_url = 'https://github.com/sowemail/'
 dir_path = os.path.abspath(os.path.dirname(__file__))
 readme_path = os.path.join(dir_path, 'README.rst')
 version_path = os.path.join(dir_path, 'VERSION.txt')
@@ -13,21 +14,28 @@ with io.open(readme_path, encoding='utf-8') as readme_file:
 with io.open(version_path, encoding='utf-8') as version_file:
     version = version_file.read().strip()
 
-base_url = 'https://github.com/sowemail/'
-
 copy_file(version_path,
           os.path.join(dir_path, 'sowerest', 'VERSION.txt'),
           verbose=False)
+
+
+def parse_requirements(filename):
+    """load requirements from a pip requirements file"""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and (not line.startswith("#") and not line.startswith('-'))]
+
 
 setup(
     name='sowerest',
     version=version,
     author='Fourat Zouari',
-    author_email='fourat@gmail.com',
-    url='{}sowerest'.format(base_url),
-    download_url='{}sowerest/tarball/{}'.format(base_url, version),
+    author_email='opensource@sowemail.com',
+    url='{}sowerest-python'.format(base_gh_url),
+    download_url='{}sowerest-python/tarball/{}'.format(base_gh_url, version),
     packages=['sowerest'],
     include_package_data=True,
+    install_requires=parse_requirements('install-requirements.pip'),
+    tests_require=parse_requirements('test-requirements.pip'),
     license='MIT',
     description='HTTP REST client, simplified for SeWeMail API',
     long_description_content_type='text/x-rst',
@@ -44,5 +52,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
